b1e1a9298ade8ceea10e54e52cea917efeb7a6fd
Maksim <maks.tishchenko@gmail.com>
2025-03-11 21:48:33 +0100

    create empty application

---
+++ IGDB/IGDB/IGDBApp.swift
@@ -0,0 +1,20 @@
+//
+//  IGDBApp.swift
+//  IGDB
+//
+//  Created by Maxim Tischenko on 11.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct IGDBApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
